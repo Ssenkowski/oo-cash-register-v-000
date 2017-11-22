420241fbfbaa3689ff42ead193597df5f1398c60
Steven Senkowski <scsenkowski@gmail.com>
2017-11-22 17:01:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,19 +7,19 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item
+  def add_item(title, price, quantity=0)
     
   end
 
   def apply_discount
-    
+
   end
-  
+
   def items
-    
+
   end
 
   def void_last_transaction
-    
+
   end
 end
